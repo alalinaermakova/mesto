8d5f8eedce7c7a07b84c618ea2cccf5db2f758f3
Alina Ermakova <marksonalina@gmail.com>
2020-11-15 01:01:46 +0000

    added new post, submit posr form, active like button, option to delete posts, open photo full size

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,125 @@
-let popup = document.querySelector('.pop-up');
-let changeButton = document.querySelector('.profile__button-edit');
-let closeButton = document.querySelector('.pop-up__button-close');
-let name = document.querySelector('.profile__name');
-let description = document.querySelector('.profile__description');
-let form = document.querySelector('.pop-up__form');
-let nameField = document.querySelector('.pop-up__input_name');
-let descriptionField = document.querySelector('.pop-up__input_description');
+const popup = document.querySelectorAll('.pop-up');
+const editProfilePopup = popup[0];
+const addPostPopup = popup[1];
+const openPhotoPopup = popup[2];
+const editProfileButton = document.querySelector('.profile__button-edit');
+const addPostButton = document.querySelector('.profile__button-add');
+const addButtonPost = document.querySelector('.pop-up__button-submit_photo');
+const closePopupButtons = document.querySelectorAll('.pop-up__button-close');
+const name = document.querySelector('.profile__name');
+const description = document.querySelector('.profile__description');
+const form = document.querySelector('.pop-up__form');
+const formPost = document.querySelector('.pop-up__form_photo');
+const nameField = document.querySelector('.pop-up__input_name');
+const descriptionField = document.querySelector('.pop-up__input_description');
+const postContainer = document.querySelector('.elements');
+const postName = document.querySelector('.pop-up__input_post-name');
+const postLink = document.querySelector('.pop-up__input_post-link');
 
-function showPopup() {
-    nameField.value = name.textContent;
-    descriptionField.value = description.textContent;
-    popup.classList.add('pop-up_edit');
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+function addPost(name, link){
+    const postTemplate = document.querySelector('#post-template').content;
+    const postElement = postTemplate.cloneNode(true);
+    const photoElement = postElement.querySelector('.element__item_input');
+    const textElement = postElement.querySelector('.element__text_input');
+    const likeButton = postElement.querySelector('.element__button-like');
+    const deleteButton = postElement.querySelector('.element__button-delete');
+
+    photoElement.src = link;
+    textElement.textContent = name;
+
+    addLikeEvent(likeButton);
+    postContainer.prepend(postElement);
+    deleteButton.addEventListener('click', deletePosts);
+    photoElement.addEventListener('click', _ => {
+        openPhoto(link, name);
+    });
+    closePopup(addPostPopup);
 }
 
-function closePopup() {
-    popup.classList.remove('pop-up_edit');
+initialCards.forEach(item => {
+    addPost(item.name, item.link);
+});
+
+function showPopup(popup) {
+    popup.classList.add('pop-up_opened')
 }
 
-function submitForm(event) {
-    event.preventDefault();
+function closePopup(popup) {
+    popup.classList.remove('pop-up_opened'); 
+}
+
+function submitEditProfileForm(evt) {
+    evt.preventDefault();
     name.textContent = nameField.value;
     description.textContent = descriptionField.value;
-    closePopup();
+    closePopup(editProfilePopup);
 }
 
-form.addEventListener('submit', submitForm);
-changeButton.addEventListener('click', showPopup);
-closeButton.addEventListener('click', closePopup);
+function submitNewPostForm(evt) {
+    evt.preventDefault();
+    const name = postName.value;
+    const link = postLink.value;
+    addPost(name, link);
+}
+
+function addLikeEvent(element) {
+    element.addEventListener('click', evt => {
+        evt.target.classList.toggle('element__button-like_active');
+    })
+};
+
+function deletePosts(evt) {
+    const eventTarget = evt.target;
+    eventTarget.closest('.element').remove();
+};
+  
+function openPhoto(photo, title) {
+    showPopup(openPhotoPopup);
+    openPhotoPopup.querySelector('.pop-up__img').src = photo;
+    openPhotoPopup.querySelector('.pop-up__post-title').textContent = title;
+};
+
+formPost.addEventListener('submit', submitNewPostForm);
+form.addEventListener('submit', submitEditProfileForm);
+editProfileButton.addEventListener('click', _ => {
+    showPopup(editProfilePopup);
+    nameField.value = name.textContent;
+    descriptionField.value = description.textContent;
+});
+addPostButton.addEventListener('click', _ => {
+    showPopup(addPostPopup);
+});
+
+closePopupButtons.forEach (button => {
+    button.addEventListener('click', _ => {
+        const buttonClosestPopup = button.closest('.pop-up');
+        closePopup(buttonClosestPopup);
+    } );
+})
