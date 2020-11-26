306473aa09c32b61972747c5d5ccc972bf3e8669
Alina Ermakova <marksonalina@gmail.com>
2020-11-26 22:17:15 +0000

    close popup witj click on overlay and esc btn, created validate.js for forms validation, validated 2 forms

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
-const popup = document.querySelectorAll('.pop-up');
-const editProfilePopup = popup[0];
-const addPostPopup = popup[1];
-const openPhotoPopup = popup[2];
+const popupList = document.querySelectorAll('.pop-up');
+const editProfilePopup = popupList[0];
+const addPostPopup = popupList[1];
+const openPhotoPopup = popupList[2];
 const editProfileButton = document.querySelector('.profile__button-edit');
 const addPostButton = document.querySelector('.profile__button-add');
 const addButtonPost = document.querySelector('.pop-up__button-submit_photo');
@@ -67,12 +67,29 @@ initialCards.forEach(item => {
     addCard(item.name, item.link);
 });
 
-function showPopup(popup) {
-    popup.classList.add('pop-up_opened')
+function closePopup(popup) {
+    popup.classList.toggle('pop-up_opened'); 
+    document.removeEventListener('click', closePopupOverlay);
+    document.removeEventListener('keydown', closePopupOnEscapeBtn);
 }
 
-function closePopup(popup) {
-    popup.classList.remove('pop-up_opened'); 
+function closePopupOverlay(evt) {
+    if (evt.target.classList.contains('pop-up')){
+        closePopup(evt.target);
+    }
+};
+
+function closePopupOnEscapeBtn(evt) {
+    if (evt.key === 'Escape') {
+        const shownPopup = Array.from(popupList).find(element => element.classList.contains('pop-up_opened'));
+        closePopup(shownPopup);
+    } 
+};
+
+function showPopup(popup) {
+    popup.classList.add('pop-up_opened');
+    document.addEventListener('click', closePopupOverlay);
+    document.addEventListener('keydown', closePopupOnEscapeBtn);
 }
 
 function submitEditProfileForm(evt) {
@@ -112,9 +129,11 @@ editProfileButton.addEventListener('click', _ => {
     showPopup(editProfilePopup);
     nameField.value = name.textContent;
     descriptionField.value = description.textContent;
+    enableValidation();
 });
 addPostButton.addEventListener('click', _ => {
     showPopup(addPostPopup);
+    enableValidation();
 });
 
 closePopupButtons.forEach (button => {
@@ -122,4 +141,5 @@ closePopupButtons.forEach (button => {
         const buttonClosestPopup = button.closest('.pop-up');
         closePopup(buttonClosestPopup);
     } );
-})
+});
+
